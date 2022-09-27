6c593721f9beeacfbcd053ece1197a3ac4f941f0
a0987143311 <a0987143311@gmail.com>
2022-09-27 21:52:55 +0800

    Add project files.

---
+++ Adapter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+var bmp = new Bitmap("C:/resultImage.jpg");
+IProductInspector inspector = new IOpenCVProductInspector(new OpenCVInspector());
+var res = inspector.Inspect(bmp);
+
+
+Console.WriteLine(res);
+
+
+interface IProductInspector
+{
+    public string Inspect(Bitmap bmp);
+}
+
+class HalconInspector : IProductInspector
+{
+    public string Inspect(Bitmap bmp)
+    {
+        return "result by halcon";
+    }
+}
+
+class IOpenCVProductInspector : IProductInspector
+{
+    private readonly OpenCVInspector _openCVInspector;
+
+    public IOpenCVProductInspector(OpenCVInspector openCVInspector)
+    {
+        _openCVInspector = openCVInspector;
+    }
+
+    public string Inspect(Bitmap bmp)
+    {
+        byte[] byts = ConvertToByts(bmp);
+
+        return _openCVInspector.Inspect(byts);
+    }
+
+    private byte[] ConvertToByts(Bitmap bmp)
+    {
+        byte[] byts = new byte[] { 1, 1 };
+        return byts;
+    }
+}
+
+class OpenCVInspector
+{
+    public string Inspect(byte[] byts)
+    {
+        return "result by ";
+    }
+}
